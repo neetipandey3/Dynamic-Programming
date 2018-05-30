f3769487ec85ceb872f35d8e833944b44df2af75
Neeti <neetipandey3@gmail.com>
2018-05-30 15:17:20 -0700

    merge

--- max-product-subarray.py
+++ max-product-subarray.py
@@ -17,38 +17,50 @@ Output: 0
 Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
 '''
-
+import math
 class MaxProductSubArray:
     def getMaxProdSubarray(self, nums: list):
         len_nums = len(nums)
-        max_products = [0 for i in range(len_nums)]
-        min_products = [0 for i in range(len_nums)]
-
-        max_products[0] = min_products[0] = max_product = nums[0]
-
 
         #product_tbl[1] = max(nums[0] * nums[1], nums[1])
+        max_prod = [0 for i in range(len_nums)]
+        min_prod = [0 for i in range(len_nums)]
+        min_prod = [0 for i in range(len_nums)]
+        max_prod[0] = nums[0]
+        min_prod[0] = nums[0]
+        max_result = nums[0]
         '''
         Need to handle two cases:
         1. max positive product thus far
         2. max negative product thus far, expecting a negative number in the subsequent index
         '''
-        for i in range(1, (len_nums)):
+        for i in range(1, len_nums):
             if nums[i] > 0:
-                max_products[i] = max(max_products[i-1]*nums[i], nums[i])
-                min_products[i] = min(max_products[i - 1] * nums[i], nums[i])
-            else:
-                max_products[i] = min(max_products[i - 1] * nums[i], nums[i])
-                min_products[i] = max(max_products[i - 1] * nums[i], nums[i])
-            max_product = max(max_product, max_products[i])
+                max_prod[i] = max(max_prod[i-1] * nums[i], nums[i])
+                min_prod[i] = min(min_prod[i-1] * nums[i], nums[i])
+            elif nums[i] == 0:
 
-        return max_product
+                #reset all the values to 1
+                max_prod[i] = 0
+                min_prod[i] = 0
+            else:
+                max_prod[i] = max(min_prod[i-1] * nums[i], nums[i])
+                min_prod[i] = min(max_prod[i-1] * nums[i], nums[i])
+
+
+            #min_result = min(min_product, min_result)
+            max_result = max(max_prod[i], max_result)
+
+        return max_result
 
 def main():
     max_prod_subarray = MaxProductSubArray()
     print(max_prod_subarray.getMaxProdSubarray([2, 3, -2, 4]))
     print(max_prod_subarray.getMaxProdSubarray([-1, 0, -1]))
     print(max_prod_subarray.getMaxProdSubarray([-2, 3, -4]))  #Negative product scenario
+    print(max_prod_subarray.getMaxProdSubarray([-4, -3]))
+    print(max_prod_subarray.getMaxProdSubarray([0, 2]))
+    print(max_prod_subarray.getMaxProdSubarray([2,-5,-2,-4,3]))
 
 if __name__ == "__main__":
     main()
